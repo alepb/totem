b101c63f372a522b2ce65fb43eff0551fa5e81e2
alepb <71177312+alepb@users.noreply.github.com>
2020-09-12 17:26:46 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".thumbnail1").hover(function(){
+    $(this).css("background-color","#d4d4d4");
+    },function(){
+    $(this).css("background-color","#ffffff");
+  });
+});
+
